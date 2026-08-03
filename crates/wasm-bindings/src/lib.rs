db4c4de3c2eb4ed8b0922287a598818c80f1b2d2
LMX Blade <devart.blake@gmail.com>
2026-08-03 10:54:14 -0400

    format WASM bindings

--- crates/wasm-bindings/src/lib.rs
+++ crates/wasm-bindings/src/lib.rs
@@ -1,3 +1,6 @@
 use wasm_bindgen::prelude::*;
+
 #[wasm_bindgen]
-pub fn apply_gain(input: Vec<f32>, amount: f32) -> Vec<f32> { synaptix_dsp::gain(&input, amount) }
+pub fn apply_gain(input: Vec<f32>, amount: f32) -> Vec<f32> {
+    synaptix_dsp::gain(&input, amount)
+}
